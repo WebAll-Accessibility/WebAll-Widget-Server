5a74c0778a43f8d39833863b689bd11478507099
= <OxyGen05@outlook.it>
2023-06-27 15:57:41 +0200

    link fixes

--- server/static/js/weball-features.js
+++ server/static/js/weball-features.js
@@ -131,7 +131,7 @@ const evidenziaLink = () => {
         }
     });
 
-    toggleClass('caratteri-dislessia', 'selected');
+    toggleClass('evidenzia-link', 'selected');
 }
 
 const zoom = () => {
