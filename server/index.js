24d3cca065fe5b8e01092722089a8941626c39d5
= <OxyGen05@outlook.it>
2023-07-03 22:19:55 +0200

    test

--- server/index.js
+++ server/index.js
@@ -22,22 +22,6 @@ service.use(express.json());
 service.use(cors());
 frontend.use(express.static(path.join(__dirname, './frontend')));
 
-service.use((req, res, next) => {
-    if (req.method === 'GET') {
-        const referrer = req.get('Referrer');
-        
-        checkUser(referrer, (user) => {
-            if (!(user && user.length > 0)) {
-                console.warn(`Refused connection from ${referrer}`);
-            } else {
-                console.log(`Accepted connection from ${referrer}`);
-            }
-        });
-    }
-
-    return next();
-});
-
 
 // Utility functions
 const checkUser = (url, callback) => {
@@ -87,7 +71,18 @@ service.listen(8081, () => {
 });
 
 service.get('*', (req, res) => {
-    if (req.destroyed || res.destroyed) return;
+    const referrer = req.get('Referrer');
+    
+    checkUser(referrer, (user) => {
+        if (!(user && user.length > 0)) {
+            console.warn(`Refused connection from ${referrer}`);
+            res.status(403).end();
+            return;
+        }
+
+        console.log(`Accepted connection from ${referrer}`);
+    });
+    
     
     if (!fs.existsSync(path.join(__dirname, `./service${req.path}`))) {
         res.status(404).end();
