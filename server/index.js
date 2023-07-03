c3e8de87acc3b3c939cade6250dc5bc645993ae4
= <OxyGen05@outlook.it>
2023-07-03 22:01:13 +0200

    Shoot

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ service.use((req, res, next) => {
         checkUser(referrer, (user) => {
             if (!(user && user.length > 0)) {
                 console.warn(`Refused connection from ${referrer}`);
-                res.status(403);
+                res.sendStatus(403);
                 res.destroy();
                 req.destroy();
             } else {
