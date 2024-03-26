bf1a510061d9e531b3ae91a939c8402c1ab9110a
Alessandro-Salerno <OxyGen05@outlook.it>
2024-03-26 19:49:13 +0100

    bruh

--- server/service/bootstrap.js
+++ server/service/bootstrap.js
@@ -1,5 +1,5 @@
 var s = document.createElement('script');
 s.type = 'text/javascript';
-s.src = 'http://127.0.0.1:8081/init.js';
+s.src = 'http://52.47.171.54:8081/init.js';
 s.async = true;
 document.getElementsByTagName('head')[0].appendChild(s);
