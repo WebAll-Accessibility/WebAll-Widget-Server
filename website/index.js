f55fb529357464a2d2cd3cc7c99803f7f5fc6c60
Alessandro Salerno <67583725+Alessandro-Salerno@users.noreply.github.com>
2024-05-20 15:08:28 +0200

    Delete website directory

--- website/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const app = express()
-app.use(express.static(path.join(__dirname, './static')));
-
-app.listen(8080, () => {
-});
