77df4ac191a69eaf12bcf0f8a24786213aa81cff
Alessandro-Salerno <OxyGen05@outlook.it>
2024-05-25 17:49:47 +0200

    est

--- src/service/bootstrap.js
+++
@@ -1,5 +0,0 @@
-var s = document.createElement('script');
-s.type = 'text/javascript';
-s.src = '__WA_SERVER_ADDRESS/service/init.js';
-s.async = true;
-document.getElementsByTagName('head')[0].appendChild(s);
