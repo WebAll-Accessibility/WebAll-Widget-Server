bb9603ac301adee31e314b16f324a3cd0d2e443c
Alessandro-Salerno <OxyGen05@outlook.it>
2024-05-26 17:28:41 +0200

    status

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+rm db/created > /dev/null
 touch db/weball.tmp
 cat db/weball.sql >> db/weball.tmp
 echo "\nexit\n" >> db/weball.tmp
@@ -5,3 +6,4 @@ echo "\nexit\n" >> db/weball.tmp
 mysql -u $1 -p < db/weball.tmp
 
 rm db/weball.tmp
+touch db/created
